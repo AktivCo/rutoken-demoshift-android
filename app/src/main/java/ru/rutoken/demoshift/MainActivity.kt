f3f239bd5a62a20efa13298bd30d4a38ee0057e8
Lashin Andrey <lashin@rutoken.ru>
2020-04-21 21:42:43 +0300

    Make ActionBar the same as in Demobank

--- app/src/main/java/ru/rutoken/demoshift/MainActivity.kt
+++ app/src/main/java/ru/rutoken/demoshift/MainActivity.kt
@@ -2,33 +2,16 @@ package ru.rutoken.demoshift
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import ru.rutoken.demoshift.databinding.FragmentUserBinding
-import ru.rutoken.demoshift.user.User
-import java.text.SimpleDateFormat
-import java.util.*
+import ru.rutoken.demoshift.databinding.ActivityMainBinding
+import ru.rutoken.demoshift.databinding.ToolbarBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: FragmentUserBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = FragmentUserBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        val user = User(
-            0,
-            "Иванов Иван Иванович",
-            "Генеральный директор",
-            "ООО Организация больших и малых закупок",
-            Date()
-        )
-
-        binding.userFullName.text = user.fullName
-        binding.userPosition.text = user.position ?: getString(R.string.field_not_set)
-        binding.userOrganization.text = user.organization ?: getString(R.string.field_not_set)
-        binding.userCertificateExpires.text = if (user.certificateExpires != null) SimpleDateFormat(
-            "d MMMM yyyy",
-            Locale.getDefault()
-        ).format(user.certificateExpires) else getString(R.string.field_not_set)
+        setSupportActionBar(ToolbarBinding.inflate(layoutInflater).toolbar)
     }
 }
