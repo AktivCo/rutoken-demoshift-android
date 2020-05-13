c4fd2767620d0a5ac7d50b31622e9b489fa2d5cf
Elizaveta Prokhorova <Prohorova@rutoken.ru>
2020-05-13 10:37:12 +0300

    Fill SignFragment UI with a PDF file

--- app/src/main/java/ru/rutoken/demoshift/ui/sign/SignFragment.kt
+++ app/src/main/java/ru/rutoken/demoshift/ui/sign/SignFragment.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.github.barteksc.pdfviewer.scroll.DefaultScrollHandle
 import ru.rutoken.demoshift.databinding.FragmentSignBinding
+
 
 class SignFragment : Fragment() {
     override fun onCreateView(
@@ -14,6 +16,9 @@ class SignFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val binding = FragmentSignBinding.inflate(inflater)
+        binding.signPdfView.fromAsset("sign_document.pdf")
+            .scrollHandle(DefaultScrollHandle(requireContext()))
+            .load()
         return binding.root
     }
 }
