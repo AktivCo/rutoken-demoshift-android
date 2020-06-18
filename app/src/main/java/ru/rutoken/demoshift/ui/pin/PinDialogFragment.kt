8b1a0c61f27b159040f7dea9a33ca40aac1a3eba
Mikhail Fedotov <fedotov@rutoken.ru>
2020-06-18 22:52:30 +0300

    ui: Confirm a PIN code by a keyboard in a PinDialog
    Now user can confirm PIN code by tapping an enter button on the keyboard. 
    Also this UX change saves one click in a landscape mode.

--- app/src/main/java/ru/rutoken/demoshift/ui/pin/PinDialogFragment.kt
+++ app/src/main/java/ru/rutoken/demoshift/ui/pin/PinDialogFragment.kt
@@ -3,6 +3,7 @@ package ru.rutoken.demoshift.ui.pin
 import android.app.Dialog
 import android.content.DialogInterface.BUTTON_POSITIVE
 import android.os.Bundle
+import android.view.inputmethod.EditorInfo
 import androidx.core.os.bundleOf
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.DialogFragment
@@ -15,31 +16,43 @@ class PinDialogFragment : DialogFragment() {
     private lateinit var binding: DialogFragmentPinBinding
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        val builder = MaterialAlertDialogBuilder(context)
         binding = DialogFragmentPinBinding.inflate(layoutInflater)
-        builder.setView(binding.root)
 
-        builder.setPositiveButton(getString(R.string.pin_continue)) { _, _ ->
-            val pin = binding.pinEditText.text.toString()
-            setFragmentResult(DIALOG_RESULT_KEY, bundleOf(PIN_KEY to pin))
-        }
+        val dialog = MaterialAlertDialogBuilder(requireContext())
+            .setView(binding.root)
+            .setPositiveButton(getString(R.string.pin_continue)) { _, _ -> setResult() }
+            .setNegativeButton(getString(android.R.string.cancel)) { dialog, _ -> dialog.cancel() }
+            .create()
 
-        builder.setNegativeButton(getString(android.R.string.cancel)) { dialog, _ ->
-            dialog.cancel()
-        }
-
-        val dialog = builder.create()
         dialog.setOnShowListener {
             dialog.getButton(BUTTON_POSITIVE).isEnabled =
                 binding.pinEditText.text?.isNotEmpty() ?: false
         }
 
-        binding.pinEditText.addTextChangedListener {
-            dialog.getButton(BUTTON_POSITIVE).isEnabled =
-                binding.pinEditText.text?.isNotEmpty() ?: false
+        with(binding.pinEditText) {
+            addTextChangedListener {
+                dialog.getButton(BUTTON_POSITIVE).isEnabled =
+                    text?.isNotEmpty() ?: false
+            }
+            setOnEditorActionListener { _, actionId, _ ->
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    if (text?.isNotEmpty() == true) {
+                        setResult()
+                        dismiss()
+                    }
+                    true
+                } else {
+                    false
+                }
+            }
         }
 
         return dialog
+    }
+
+    private fun setResult() {
+        val pin = binding.pinEditText.text.toString()
+        setFragmentResult(DIALOG_RESULT_KEY, bundleOf(PIN_KEY to pin))
     }
 
     companion object {
