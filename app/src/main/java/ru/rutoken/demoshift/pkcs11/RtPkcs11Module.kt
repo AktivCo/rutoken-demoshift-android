f3cf5fb963cf2fd42fc33ee06bf2fc0503fedefa
Lashin Andrei <lashin@rutoken.ru>
2022-04-11 14:13:31 +0300

    Revert "Revert "Update pkcs11wrapper dependency to version 2.1.0""
    This reverts commit 3c99d7c5c0ed6f88f31754dda7b52e4d46087aab.
    
    Resolve: SHIFTDROID-74

--- app/src/main/java/ru/rutoken/demoshift/pkcs11/RtPkcs11Module.kt
+++ app/src/main/java/ru/rutoken/demoshift/pkcs11/RtPkcs11Module.kt
@@ -8,11 +8,9 @@ package ru.rutoken.demoshift.pkcs11
 
 import com.sun.jna.Native
 import ru.rutoken.pkcs11jna.RtPkcs11
-import ru.rutoken.pkcs11wrapper.lowlevel.jna.Pkcs11JnaLowLevelFactory
 import ru.rutoken.pkcs11wrapper.main.Pkcs11BaseModule
-import ru.rutoken.pkcs11wrapper.rutoken.constant.RtPkcs11MechanismType
-import ru.rutoken.pkcs11wrapper.rutoken.constant.RtPkcs11ReturnValue
-import ru.rutoken.pkcs11wrapper.rutoken.jna.RtPkcs11JnaLowLevelApi
+import ru.rutoken.pkcs11wrapper.rutoken.lowlevel.jna.RtPkcs11JnaLowLevelApi
+import ru.rutoken.pkcs11wrapper.rutoken.lowlevel.jna.RtPkcs11JnaLowLevelFactory
 import ru.rutoken.pkcs11wrapper.rutoken.main.RtPkcs11Api
 import ru.rutoken.pkcs11wrapper.rutoken.main.RtPkcs11HighLevelFactory
 
@@ -21,14 +19,6 @@ import ru.rutoken.pkcs11wrapper.rutoken.main.RtPkcs11HighLevelFactory
  * Use this class as entry point to pkcs11wrapper.
  */
 class RtPkcs11Module(name: String = "rtpkcs11ecp") : Pkcs11BaseModule(
-    RtPkcs11Api(
-        RtPkcs11JnaLowLevelApi(
-            Native.load(name, RtPkcs11::class.java),
-            Pkcs11JnaLowLevelFactory.Builder()
-                .setReturnValueVendorFactory(RtPkcs11ReturnValue.Factory())
-                .setMechanismTypeVendorFactory(RtPkcs11MechanismType.Factory())
-                .build()
-        )
-    ),
+    RtPkcs11Api(RtPkcs11JnaLowLevelApi(Native.load(name, RtPkcs11::class.java), RtPkcs11JnaLowLevelFactory())),
     RtPkcs11HighLevelFactory()
 )
