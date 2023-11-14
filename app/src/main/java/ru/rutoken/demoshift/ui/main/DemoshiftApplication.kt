f55dc5d75c744298ea03f37c1e2c14eb49aff444
Evgenii Potapov <potapov@rutoken.ru>
2023-11-14 16:00:26 +0300

    Connect to Rutoken via local pcsc scheme
    This type of Rutoken integration does not need the Rutoken Control
    Panel.

--- app/src/main/java/ru/rutoken/demoshift/ui/main/DemoshiftApplication.kt
+++ app/src/main/java/ru/rutoken/demoshift/ui/main/DemoshiftApplication.kt
@@ -6,21 +6,35 @@
 package ru.rutoken.demoshift.ui.main
 
 import android.app.Application
+import android.widget.Toast
 import org.koin.android.ext.android.get
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 import ru.rutoken.demoshift.koin.koinModule
 import ru.rutoken.demoshift.tokenmanager.TokenManager
-import ru.rutoken.rtpcsc.RtPcsc
+import ru.rutoken.rtpcscbridge.RtPcscBridge
+import ru.rutoken.rttransport.RtTransport
 
 class DemoshiftApplication : Application() {
+    private val readerListObserver = object : RtTransport.PcscReaderObserver {
+        override fun onReaderAdded(pcscReader: RtTransport.PcscReader) {
+            Toast.makeText(applicationContext, "new Reader: " + pcscReader.name, Toast.LENGTH_SHORT).show()
+        }
+
+        override fun onReaderRemoved(pcscReader: RtTransport.PcscReader) {
+            Toast.makeText(applicationContext, "lost Reader: " + pcscReader.name, Toast.LENGTH_SHORT).show()
+        }
+    }
     override fun onCreate() {
         super.onCreate()
         startKoin {
             androidContext(this@DemoshiftApplication)
             modules(koinModule)
         }
-        RtPcsc.setAppContext(this)
+        RtPcscBridge.enableDebugLogs()
+        RtPcscBridge.setAppContext(this)
+        RtPcscBridge.getTransport().addPcscReaderObserver(readerListObserver)
+        RtPcscBridge.getTransportExtension().attachToLifecycle(this)
         get<TokenManager>()
     }
 }
