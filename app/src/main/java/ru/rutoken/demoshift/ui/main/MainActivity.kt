5ea67991e6269698b745c1081e61f4264b0e1350
Elizaveta Prokhorova <Prohorova@rutoken.ru>
2023-12-12 10:53:36 +0300

    ui: Add BLUETOOTH_CONNECT permission request for Android 12+
    Resolve: SHIFTDROID-90

--- app/src/main/java/ru/rutoken/demoshift/ui/main/MainActivity.kt
+++ app/src/main/java/ru/rutoken/demoshift/ui/main/MainActivity.kt
@@ -5,22 +5,59 @@
 
 package ru.rutoken.demoshift.ui.main
 
+import android.Manifest.permission.BLUETOOTH_CONNECT
+import android.content.pm.PackageManager.PERMISSION_DENIED
+import android.os.Build
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import org.koin.android.ext.android.get
-import org.koin.androidx.viewmodel.ext.android.getViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
+import ru.rutoken.demoshift.R
 import ru.rutoken.demoshift.databinding.ActivityMainBinding
 import ru.rutoken.demoshift.pkcs11.Pkcs11Launcher
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var viewModel: MainViewModel
+    private val mainViewModel: MainViewModel by viewModel()
+    private val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) {}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        viewModel = getViewModel()
         lifecycle.addObserver(get<Pkcs11Launcher>())
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            mainViewModel.bluetoothPermissionRequestDone.observe(this) { bluetoothPermissionRequestDone ->
+                if (bluetoothPermissionRequestDone)
+                    return@observe
+
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this, BLUETOOTH_CONNECT))
+                    showBluetoothPermissionRationale()
+                else if (ContextCompat.checkSelfPermission(this, BLUETOOTH_CONNECT) == PERMISSION_DENIED)
+                    launchBluetoothPermissionRequest()
+            }
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.S)
+    private fun showBluetoothPermissionRationale() {
+        MaterialAlertDialogBuilder(this)
+            .setMessage(R.string.ask_bluetooth_permission)
+            .setPositiveButton(android.R.string.ok) { _, _ -> launchBluetoothPermissionRequest() }
+            .setOnCancelListener { _ -> mainViewModel.finishBluetoothPermissionRequest() }
+            .create()
+            .show()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.S)
+    private fun launchBluetoothPermissionRequest() {
+        requestPermissionLauncher.launch(BLUETOOTH_CONNECT)
+        mainViewModel.finishBluetoothPermissionRequest()
     }
 }
--- app/src/main/java/ru/rutoken/demoshift/ui/main/MainViewModel.kt
+++ app/src/main/java/ru/rutoken/demoshift/ui/main/MainViewModel.kt
@@ -5,11 +5,25 @@
 
 package ru.rutoken.demoshift.ui.main
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 import ru.rutoken.demoshift.pkcs11.Pkcs11Launcher
 
 class MainViewModel(launcher: Pkcs11Launcher) : ViewModel() {
+    private val _bluetoothPermissionRequestDone = MutableLiveData(false)
+
+    val bluetoothPermissionRequestDone: LiveData<Boolean> get() = _bluetoothPermissionRequestDone
+
     init {
         launcher.launchPkcs11()
+    }
+
+    fun finishBluetoothPermissionRequest() {
+        viewModelScope.launch {
+            _bluetoothPermissionRequestDone.value = true
+        }
     }
 }
